06d42e7a41086695c5797ccef7963f8f448a226f
Yesitsmepuri <rpuri1@students.towson.edu>
2023-09-26 19:35:59 -0400

    Login Page created
    Created Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+<<<<<<< Updated upstream
+=======
+import {Navbar} from "./Pages/Navbar";
+import { Routes,Route } from "react-router-dom";
+import { HomePage } from "./Pages/HomePage";
+import { Myjournal } from "./Pages/myjournal";
+import { LoginPage } from "./Pages/login";
+
+
+>>>>>>> Stashed changes
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -13,10 +23,20 @@ function App() {
 
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
+=======
+      <Navbar/>
+      <Routes>
+        <Route path = "/" element = {<HomePage/>} />
+        <Route path = "/Myjournal" element = {<Myjournal/>} />
+        <Route path = "/LoginPage" element = {<LoginPage/>} />
+      </Routes>
+      
+>>>>>>> Stashed changes
     </div>
   );
 }
