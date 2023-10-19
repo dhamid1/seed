33a7d093f5a60e3a4957f7383b4cdc9a19d52d37
Jose6880 <joser6880@gmail.com>
2023-10-19 18:05:10 -0400

    signupdone
    signup done

--- client/src/Pages/Navbar.jsx
+++ client/src/Pages/Navbar.jsx
@@ -1,3 +1,37 @@
+// import React from 'react';
+// import { Link, NavLink } from 'react-router-dom';
+// import './Navbar.css';
+
+// export const Navbar = ({ isLoggedIn }) => {
+//   return (
+//     <nav>
+//       <Link to="/" className="title">
+//         SEED
+//       </Link>
+//       <ul>
+//         {isLoggedIn && (
+//           <li>
+//             <NavLink to="/Myjournal">My Journal</NavLink>
+//           </li>
+//         )}
+//         <li>
+//           <NavLink to="/LoginPage">Log In</NavLink>
+//         </li>
+//         {isLoggedIn && (
+//           <li>
+//             <NavLink to="/Account">Account</NavLink>
+//           </li>
+//         )}
+//         {isLoggedIn && (
+//           <li>
+//             <NavLink to="/Profile">Profile</NavLink>
+//           </li>
+//         )}
+//       </ul>
+//     </nav>
+//   );
+// };
+/////////////////////
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './Navbar.css';
@@ -9,7 +43,7 @@ export const Navbar = ({ isLoggedIn }) => {
         SEED
       </Link>
       <ul>
-        {isLoggedIn && (
+        {(
           <li>
             <NavLink to="/Myjournal">My Journal</NavLink>
           </li>
@@ -17,19 +51,14 @@ export const Navbar = ({ isLoggedIn }) => {
         <li>
           <NavLink to="/LoginPage">Log In</NavLink>
         </li>
-        {isLoggedIn && (
+        { (
           <li>
             <NavLink to="/Account">Account</NavLink>
           </li>
         )}
-        {isLoggedIn ? (
+        {(
           <li>
             <NavLink to="/Profile">Profile</NavLink>
-          </li>
-        ) : (
-          // Optionally, you can add a "Register" link when not logged in.
-          <li>
-            <NavLink to="/Register">Register</NavLink>
           </li>
         )}
       </ul>
--- client/src/Pages/login.jsx
+++ client/src/Pages/login.jsx
@@ -1,16 +1,13 @@
-
-import { useState } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, TextField, Button, styled, Typography } from '@mui/material';
-
 import Images from './Images/Logo.png';
 import { API } from '../../src/Pages/service/api.js';
 
 const Component = styled(Box)`
-width: 500px;
-margin: auto;
-box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.7);
-margin-top: 75px;
+  width: 500px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgba(0, 0, 0, 0.7);
+  margin-top: 75px;
 `;
 
 const Image = styled('img')({
@@ -18,38 +15,35 @@ const Image = styled('img')({
   margin: 'auto',
   display: 'flex',
   padding: '50px 0 0',
-
 });
 
 const Wrapper = styled(Box)`
-padding: 25px 35px;
-display: flex;
-flex: 1;
-flex-direction: column;
-& > div , & > p {
-  margin-top: 20px;
-
-  
-
-}
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  & > div,
+  & > p {
+    margin-top: 20px;
+  }
 `;
 
 const LoginButton = styled(Button)`
-text-transform: none;
-background: #FB641B;
-color: #fff;
-height: 45px;
-border-radius: 2px;
-margin-top: 10px;
+  text-transform: none;
+  background: #FB641B;
+  color: #fff;
+  height: 45px;
+  border-radius: 2px;
+  margin-top: 10px;
 `;
 
 const SignupButton = styled(Button)`
-text-transform: none;
-background: #fff;
-color: #2874f0;
-height: 45px;
-border-radius: 2px;
-box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 45px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
 `;
 
 const Error = styled(Typography)`
@@ -60,108 +54,108 @@ const Error = styled(Typography)`
   font-weight: 600;
 `;
 
-const signupInitialValues ={
-  name: '',
-  username: '',
-  password: '',
-};
-
-
 export const LoginPage = () => {
-
   const [account, toggleAccount] = useState('login');
-  const [signup, setSignup] = useState(JSON.stringify(signupInitialValues));
-  const {error, setError} = useState('');
+  const [signup, setSignup] = useState({
+    name: '',
+    username: '',
+    password: '',
+  });
+  const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // State for showing/hiding password
+
   const toggleSignup = () => {
     account === 'signup' ? toggleAccount('login') : toggleAccount('signup');
-  }
-  
-    const onInputChange =(e) =>{
-      setSignup({ ...signup, [e.target.name]: e.target.value});
+  };
+
+  const onInputChange = (e) => {
+    setSignup({ ...signup, [e.target.name]: e.target.value });
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword); // Toggle password visibility
+  };
+
+  const validateFields = () => {
+    // Check if any of the fields are empty
+    if (!signup.name || !signup.username || !signup.password) {
+      setError('All fields are required.');
+      return false;
     }
-    
-    console.log('Error: help me' );
-    const signupUser = async () => {
+    return true;
+  };
+
+  const signupUser = async () => {
+    // Validate the fields before making the signup request
+    if (!validateFields()) {
+      return;
+    }
+
+    try {
       let response = await API.userSignup(signup);
-     
-      if (response.isSuccess){
-         setError('hello');
-        setSignup(JSON.stringify(signupInitialValues));
-         toggleAccount('login');
-       
-      } else{
+      console.log('API Response:', response); // Log the response
+      if (response.isSuccess) {
+        setError('Account created successfully.');
+        setSignup({
+          name: '',
+          username: '',
+          password: '',
+        });
+        toggleAccount('login');
+      } else {
         setError('Something went wrong! Please try again later.');
       }
- 
-  }
-  
-  
-  
-  
-  // const onInputChange =(e) =>{
-  //   setSignup({ ...signup, [e.target.name]: e.target.value});
-  // }
-  // console.log('Error: help me' );
-  // const signupUser = async () => {
-  //   let response = await API.userSignup(signup);
-  //   if (response.isSuccess){
-  //     setError('');
-  //     setSignup(JSON.stringify(signupInitialValues));
-  //      toggleAccount('login');
-     
-  //   } else{
-  //     setError('Something went wrong! Please try again later.');
-  //   }
-    
-  // const signupUser = async () => {
-  //   try {
-  //     let response = await API.userSignup(signup);
-  //     if (response.isSuccess) {
-  //       setError('');
-  //       setSignup(JSON.stringify(signupInitialValues));
-  //       toggleAccount('login');
-  //     } else {
-  //       setError('Something went wrong! Please try again later.');
-  //     }
-  //   } catch (error) {
-  //     // Handle the error here, e.g., display an error message or log it
-  //     console.error('Error during signup:', error);
-  //   }
-  // }
+    } catch (error) {
+      console.error('Error during signup:', error);
+      setError('An error occurred while signing up. Please try again later. Error: ' + error.message);
+    }
+  };
 
   return (
-
     <Component>
       <Box>
         <Image src={Images} alt="login" />
       </Box>
 
-      {
-        account === 'login' ?
-          <Wrapper>
-            <TextField variant="standard" label="UserName" />
-            <TextField variant="standard" label="Password" />
-            { error && <Error>{error}</Error>}
+      {account === 'login' ? (
+        <Wrapper>
+          <TextField variant="standard" label="UserName" />
+          <TextField
+            variant="standard"
+            label="Password"
+            type={showPassword ? 'text' : 'password'} // Toggle type based on showPassword state
+          />
+          <Button variant="contained" onClick={togglePasswordVisibility}>
+            {showPassword ? 'Hide' : 'Show'} {/* Toggle button text based on showPassword state */}
+          </Button>
+          {error && <Error>{error}</Error>}
 
-
-            <LoginButton variant="contained">Login</LoginButton>
-            <Typography style={{ textAlign: 'center' }}>OR</Typography>
-            <SignupButton onClick = {() => toggleSignup()}>Create an Account</SignupButton>
-          </Wrapper>
-          :
-
-          <Wrapper>
-            <TextField variant="standard" onChange={(e)=> onInputChange(e)} name = 'name' label="Enter Name" />
-            <TextField variant="standard" onChange={(e)=> onInputChange(e)} name = 'username'label="User Name" />
-            <TextField variant="standard" onChange={(e)=> onInputChange(e)} name = 'password'label="Password" />
-
-            { error && <Error>{error}</Error>}
-            <SignupButton onClick={() => signupUser()}>Signup</SignupButton>
-            <Typography style={{ textAlign: 'center' }}>OR</Typography>
-            <LoginButton variant="contained" onClick={() => toggleSignup()}>Already have an account</LoginButton>
-          </Wrapper>
-      }
-
+          <LoginButton variant="contained">Login</LoginButton>
+          <Typography style={{ textAlign: 'center' }}>OR</Typography>
+          <SignupButton onClick={toggleSignup}>Create an Account</SignupButton>
+        </Wrapper>
+      ) : (
+        <Wrapper>
+          <TextField variant="standard" onChange={onInputChange} name="name" label="Enter Name" />
+          <TextField variant="standard" onChange={onInputChange} name="username" label="User Name" />
+          <TextField
+            variant="standard"
+            onChange={onInputChange}
+            name="password"
+            label="Password"
+            type={showPassword ? 'text' : 'password'} // Toggle type based on showPassword state
+          />
+          <Button variant="contained" onClick={togglePasswordVisibility}>
+            {showPassword ? 'Hide' : 'Show'} {/* Toggle button text based on showPassword state */}
+          </Button>
+          {error && <Error>{error}</Error>}
+          <SignupButton onClick={signupUser}>Sign Up</SignupButton>
+          <Typography style={{ textAlign: 'center' }}>OR</Typography>
+          <LoginButton variant="contained" onClick={toggleSignup}>
+            Already have an account
+          </LoginButton>
+        </Wrapper>
+      )}
     </Component>
-  )
-}
+  );
+};
