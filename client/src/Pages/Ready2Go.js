00300432b6bfa5519aab50e0dc44d415914d600e
Novoa Champion <144487949+novoachampion@users.noreply.github.com>
2023-11-05 03:53:16 -0500

    deleted test code
    deleted some test code from ready2Go

--- client/src/Pages/Ready2Go.js
+++ client/src/Pages/Ready2Go.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './Ready2Go.css'; // Import a CSS file for styling
 
-// function oneReady(){
-//     console.log("hello");
-// };
-
 const Ready2Go = () => {
   return (
     <div className="ready2go">
@@ -12,14 +8,13 @@ const Ready2Go = () => {
     Hello World
     </header>
     <div>
-        <button id = "ready" >
+        <button id = "ready2go">
             Ready?
         </button>
     </div>
     </div>
   );
 };
-//document.getElementById("ready").addEventListener("click", oneReady)
 // this page will change the home page to whatever we want! if we want to link 
 //stuff just let me know and ill look around
 export default Ready2Go;
